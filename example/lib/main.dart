6038a6be8d664ba4d067df522b4f5b979baed5d1
Tedi Tejo Yuwono <tedi.yuwono@dkatalis.com>
2020-02-26 20:53:22 +0700

    - update sample for decript document

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,6 +3,10 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 import 'package:couchbase_lite/couchbase_lite.dart';
+import 'package:encrypt/encrypt.dart' as aes;
+import 'package:crypto/crypto.dart';
+import 'package:hex/hex.dart';
+import 'package:password_hash/pbkdf2.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,8 +20,10 @@ class _MyAppState extends State<MyApp> {
   Database database;
   Replicator replicator;
   bool init = true;
-  int s ;
+  int s;
   int idx = 0;
+  final TextEditingController controller = TextEditingController();
+  String docText = '';
 
   @override
   void initState() {
@@ -35,11 +41,10 @@ class _MyAppState extends State<MyApp> {
           database, "ws://192.168.1.75:4984/spirit-bucket");
       config.replicatorType = ReplicatorType.pushAndPull;
       config.continuous = true;
-      config.channels = ['master'];
+      config.channels = ['master','spiritchannel'];
       config.pushAttributeKeyFilter = 'type';
       config.pushAttributeValuesFilter = ['localdoc'];
-      config.headers = {'Authentication' : 'Bearer token'};
-
+      config.headers = {'Authentication': 'Bearer token'};
 
       // Using self signed certificate
       replicator = Replicator(config);
@@ -54,10 +59,10 @@ class _MyAppState extends State<MyApp> {
         }
 
         print(event.status.activity.toString());
-        if (event.status.activity != ReplicatorActivityLevel.idle) {
+        if (event.status.activity == ReplicatorActivityLevel.busy ||
+            event.status.activity == ReplicatorActivityLevel.connecting) {
           return;
         }
-
 
 //        Query query = QueryBuilder.select([SelectResult.expression(Functions.count(Expression.all()))])
 //            .from(database.name)
@@ -65,8 +70,8 @@ class _MyAppState extends State<MyApp> {
 //        ResultSet qresult = await query.execute();
         int count = await database.count;
         int e = DateTime.now().millisecondsSinceEpoch;
-        final m = ((e-s)~/60000);
-        final ss = ((e-s)%60000)/1000;
+        final m = ((e - s) ~/ 60000);
+        final ss = ((e - s) % 60000) / 1000;
         result = "Document Count: $count ($m:$ss)";
         setState(() {
           _documentCount = result;
@@ -98,19 +103,6 @@ class _MyAppState extends State<MyApp> {
             children: <Widget>[
               RaisedButton(
                 onPressed: () async {
-//              Query query = QueryBuilder.select([SelectResult.all()])
-//                  .from(database.name)
-//                  .where(Expression.property("type")
-//                      .iS(Expression.string("villagecode")));
-//              ResultSet qresult = await query.execute();
-//              int count = qresult.length;
-//              int count = await database.count;
-//              var result = "Document Count: $count";
-//              setState(() {
-//                _documentCount = result;
-//              });
-//              final doc = await database.documentWithId('1111');
-//              print(doc);
                   final t = DateTime.now();
                   final id = t.millisecondsSinceEpoch.toString();
                   MutableDocument mutableDoc = MutableDocument();
@@ -122,13 +114,70 @@ class _MyAppState extends State<MyApp> {
                     mutableDoc.setString('type', 'localdoc');
                   } else {
                     mutableDoc.setString('type', 'notlocaldoc');
-
                   }
                   idx++;
                   await database.save(mutableDoc);
+                  setState(() {
+                    controller.text = id;
+                    docText = '';
+                  });
                 },
                 child: Text(_documentCount),
               ),
+              TextField(
+                controller: controller,
+              ),
+              RaisedButton(
+                  onPressed: () async {
+                    Query query = QueryBuilder.select([SelectResult.all()])
+                        .from(database.name)
+                        .where(Expression.property("id")
+                            .iS(Expression.string(controller.text)));
+                    ResultSet qresult = await query.execute();
+                    setState(() {
+                      if (qresult.isNotEmpty) {
+                        final d = qresult.elementAt(0).toMap();
+                        if (d['spirit-bucket'] != null &&
+                            d['spirit-bucket']['data'] != null) {
+                          final generator = PBKDF2(hashAlgorithm: sha1);
+                          final key = aes.Key.fromBase16(HEX.encode(generator.generateKey("123456", "123456", 1, 32)));
+                          final iv = aes.IV.fromBase16(HEX.encode(generator.generateKey("123456", "123456", 1, 16)));
+
+                          final encrypter = aes.Encrypter(aes.AES(key, mode: aes.AESMode.cbc, padding: 'PKCS7'));
+
+                          try {
+                            final decrypted = encrypter.decrypt64(
+                                d['spirit-bucket']['data'], iv: iv);
+                            d['spirit-bucket']['decripted'] = decrypted;
+                            docText = d.toString();
+                          } catch (ex) {
+                            print(ex);
+                          }
+                        } else {
+                          docText = d.toString();
+                        }
+                      } else {
+                        docText = '${controller.text} not found';
+                      }
+                    });
+//                    query = QueryBuilder.select([SelectResult.all()])
+//                        .from(database.name)
+//                        .where(Expression.property("id")
+//                        .iS(Expression.string(controller.text)));
+//                    qresult = await query.execute();
+//                    for (final q in qresult) {
+//                      final id = q.toMap();
+//                      final doc = await database.documentWithId(id['spirit-bucket']['id']);
+//                      MutableDocument tDoc =  doc.toMutable();
+//                      tDoc.setString('content', 'edited');
+//                      await database.save(tDoc);
+//                      tDoc.setString('content', 'edited again');
+//                      await database.save(tDoc);
+//                    }
+                  },
+                  child: Text('Document check'),
+              ),
+              Text('$docText'),
             ],
           ),
         ),
